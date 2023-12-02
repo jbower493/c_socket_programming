726a49ac7598abf46acf4a34322136a0abde53c5
Jamie Bower <j_bower@hotmail.co.uk>
2023-12-02 07:10:20 +0000

    feat: setup server and client and try to understand what each line is doing

---
+++ client.c
@@ -0,0 +1,78 @@
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+// #include <unistd.h>
+#define PORT 8080
+
+int main() {
+	// Server address
+	struct sockaddr_in server_address;
+	server_address.sin_family = AF_INET;
+	server_address.sin_port = htons(PORT);
+
+	// Convert socket address from plain text into numberic binary
+	int address_text_to_binary_result = inet_pton(
+		// Address family (IPv4 or IPv6)
+		AF_INET,
+		// String being passed in
+		"127.0.0.1",
+		// dst, buffer in which to store the numberic address. For this we can pass in the .sin_addr of the server address
+		&server_address.sin_addr
+	);
+
+	// Exit if the address conversion failed. Here we have to check if it's <= 0, because 0 is returned if the input string is not valid, and -1 for any other error
+	if (address_text_to_binary_result <= 0) {
+		printf("Address text to binary conversion failed\n");
+		return 1;
+	}
+
+	// Create the client socket
+	int client_socket = socket(AF_INET, SOCK_STREAM, 0);
+
+	// Exit if creation failed
+	if (client_socket < 0) {
+		printf("Client socket creation failed\n");
+		return 1;
+	}
+
+	// Attempt to connect to the server
+	int connected = connect(
+		// The client socket we're using to connect to the server with
+		client_socket,
+		// Address, address of the serer socket to connect to
+		(struct sockaddr *)&server_address,
+		// Size in bytes of the server address
+		sizeof(server_address)
+	);
+
+	// Exit if connection fails
+	if (connected < 0) {
+		printf("Connection to server failed\n");
+		return 1;
+	}
+
+	// Send a message to the server
+	char * message = "Hello from the client socket once again";
+
+	int num_of_bytes_sent = send(
+		// Client socket to send with
+		client_socket,
+		// buffer, pointer to the char * message to send
+		message,
+		// buffer size
+		strlen(message),
+		// flage
+		0
+	);
+
+	if (num_of_bytes_sent < 0) {
+		printf("Failed to sent message");
+		return 1;
+	}
+		
+	
+
+	printf("Client success\n");
+    return 0;
+}
---
+++ server.c
@@ -0,0 +1,140 @@
+// https://pubs.opengroup.org/onlinepubs/009604499/basedefs/netinet/in.h.html
+#include <netinet/in.h>
+#include <stdio.h>
+// #include <stdlib.h>
+// #include <string.h>
+// https://pubs.opengroup.org/onlinepubs/009604499/basedefs/sys/socket.h.html
+#include <sys/socket.h>
+#include <unistd.h>
+
+#define PORT 8080
+#define BACKLOG 10
+#define REQUEST_BUFFER_SIZE 1024
+
+
+int main() {
+	/***** VARIABLES *****/
+	// Boolean to use for setting socket option values
+	int socket_option_value = 1;
+
+	// Server internet socket address
+	struct sockaddr_in server_address;
+	// Protocal, IPv4
+	server_address.sin_family = AF_INET;
+	// Port number, in "network byte order" (host-to-network short)
+	server_address.sin_port = htons(PORT);
+	// Internet address, in "network byte order" (host-to-network long). Use INADDR_LOOPBACK for localhost, use INADDR_ANY for any address
+	server_address.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+
+	/***** PROGRAM START *****/
+    // Define a socket
+	int server_socket = socket(
+		// Domain (int), use AF_INET for communicating between processes on different hosts via IPV4
+		AF_INET,
+		// Communication type, use SOCK_STREAM for TCP, SOCK_DGRAM for UDP
+		SOCK_STREAM,
+		// Protocol (int), use 0 for IP (internet protocol)
+		0
+	);
+
+	// Exit if create socket failed
+	if (server_socket < 0) {
+		printf("Failed to create socket\n");
+		return 1;
+	}
+
+	// Set socket option (useful for socket reuse)
+	int set_socket_option_result = setsockopt(
+		// Socket, The socket to set options for
+		server_socket,
+		// Level, "Socket" level or "protocol" level
+		SOL_SOCKET,
+		// Option name, the name of the single option to set
+		SO_REUSEADDR,
+		// Option value (const void *), "const void *" means a pointer to some memory that should not be modified
+		&socket_option_value,
+		// Option length
+		sizeof(int)
+	);
+	
+	// Exit if set socket option failed
+	if (set_socket_option_result < 0) {
+		printf("Failed to set socket option\n");
+		return 1;
+	}
+
+	int socket_bind_result = bind(
+		// Socket (descriptor)
+		server_socket,
+		// The address to bind the socket to (protocol, port and address
+		(struct sockaddr *)&server_address,
+		// Length of the address in bytes
+		sizeof(server_address)
+	);
+
+	// Exit if socket bind failed
+	if (socket_bind_result < 0) {
+		printf("Failed to bind socket\n");
+		return 1;
+	}
+
+	// Start the socket listening for incoming connections
+	int listen_result = listen(
+		// Socket to listen with
+		server_socket,
+		// Backlog (max connections, socket will get an error if it recieves a connection when the current num of connections is already at the max
+		BACKLOG
+	);
+
+	// Exit if socket fails to listen
+	if (listen_result < 0) {
+		printf("Socket failed to listen\n");
+		return 1;
+	}
+
+	// Extract the first connection request in the pending connections queue and return a file descriptor to the connected client socket
+	int connected_client_socket = accept(
+		// Socket, listening socket on the server that we've already created, bound, and listened with
+		server_socket,
+		// address, null pointer or a pointer to a sockaddr structure to hold the address of the connecting socket
+		NULL,
+		// address_len, pointer to a variable (socklen_t) to hold length of the supplied sockaddr structure supplied in arg2, or null pointer if none was provided
+		NULL
+	);
+
+	// Exit if accept failed
+	if (connected_client_socket < 0) {
+		printf("Failded to accept connection\n");
+		return 1;
+	}
+
+	// Read fthe request data
+	char request_buffer[REQUEST_BUFFER_SIZE];
+	int num_of_bytes_read = read(
+		// Client socket to read from
+		connected_client_socket,
+		// Buffer into which to write the read data
+		request_buffer,
+		// Size to read
+		REQUEST_BUFFER_SIZE
+	);
+
+	// Exit if read failed
+	if (num_of_bytes_read < 0) {
+		printf("Faild to read request data\n");
+		return 1;
+	}
+
+	printf("Bytes read: %i\n", num_of_bytes_read);
+
+	for (int i = 0; i < num_of_bytes_read; i++) {
+		printf("Char number %i: %c\n", i, request_buffer[i]);
+	}
+
+	// Close the connected socket
+	close(connected_client_socket);
+	// Close the listening socket
+	close(server_socket);
+
+	return 0;
+}
